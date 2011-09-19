86010343e253736e5a4d6af63f78fcc54967f5c9
Takashi Miwa <sanrinsha.tm@gmail.com>
2011-09-19 12:44:20 +0900

    first commit

---
+++ setup.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>SETUP</title>
+</head>
+<body>
+<?php
+require_once('common.inc');
+//データベースに接続
+$conn = sqlite_open(DB, 0666, $error) or die($error);
+//テーブルの作成
+sqlite_query($conn, 'CREATE TABLE ' . TABLE . ' (msg_id integer primary key, name text, message text, time text)', SQLITE_ASSOC, $error) or die($error);
+
+//接続を切る
+sqlite_close($conn);
+
+echo '初期化が終わりました';
+
+?>
+</body>
+</html>
