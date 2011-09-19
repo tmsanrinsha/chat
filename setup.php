ad8c2c74e8f6bc8892076a1db79c4f4310e08a90
Takashi Miwa <sanrinsha.tm@gmail.com>
2011-09-19 15:36:19 +0900

    minor change

--- setup.php
+++ setup.php
@@ -8,7 +8,7 @@ require_once('common.inc');
 //データベースに接続
 $conn = sqlite_open(DB, 0666, $error) or die($error);
 //テーブルの作成
-sqlite_query($conn, 'CREATE TABLE ' . TABLE . ' (msg_id integer primary key, name text, message text, time text)', SQLITE_ASSOC, $error) or die($error);
+sqlite_query($conn, 'CREATE TABLE ' . TABLE . ' (msg_id INTEGER PRIMARY KEY, name TEXT, message text, time text)', SQLITE_ASSOC, $error) or die($error);
 
 //接続を切る
 sqlite_close($conn);
