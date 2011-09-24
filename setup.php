c5e2e13c01d6d995b74e808a452eb37226584947
Takashi Miwa <sanrinsha.tm@gmail.com>
2011-09-24 22:08:01 +0900

    pdo

--- setup.php
+++ setup.php
@@ -4,14 +4,24 @@
 </head>
 <body>
 <?php
-require_once('common.inc');
-//データベースに接続
-$conn = sqlite_open(DB, 0666, $error) or die($error);
-//テーブルの作成
-sqlite_query($conn, 'CREATE TABLE ' . TABLE . ' (msg_id INTEGER PRIMARY KEY, name TEXT, message text, time text)', SQLITE_ASSOC, $error) or die($error);
+require_once 'common.inc';
 
-//接続を切る
-sqlite_close($conn);
+//データベースに接続
+try {
+    $pdo = new PDO(CONNECT);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->query("CREATE TABLE " . TABLE . " (msg_id INTEGER PRIMARY KEY, name TEXT, message TEXT, time TEXT)");
+} catch (PDOException $e){
+    var_dump($e->getMessage());
+}
+
+$pdo = null;
+//$conn = sqlite_open(DB, 0666, $error) or die($error);
+////テーブルの作成
+//sqlite_query($conn, 'CREATE TABLE ' . TABLE . ' (msg_id INTEGER PRIMARY KEY, name TEXT, message text, time text)', SQLITE_ASSOC, $error) or die($error);
+//
+////接続を切る
+//sqlite_close($conn);
 
 echo '初期化が終わりました';
 
